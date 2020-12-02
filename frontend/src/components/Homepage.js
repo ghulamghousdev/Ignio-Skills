f9cc72a049e13af20ca9bcf07fb52b8fc778efd2
ghulamghousdev <itsghulamghous@gmail.com>
2020-12-02 22:22:14 +0500

    Added Homepage component

---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Homepage extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>HomePage</h1>
+            </div>
+        )
+    } 
+
+}
+
+export default Homepage;
