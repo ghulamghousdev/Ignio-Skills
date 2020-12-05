01474fe1b2c757b1d8b7aef1e5b843d5f4982b9e
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2020-12-05 09:48:15 -0800

    Changed anchors to Router Link

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import '../styles/NavBar.scss'
-function NavBar(){
-    return(
-        <div className="home-nav one-edge-shadow">
-            <div className="home-row ">
-                <div className="home-logo">Ignio Skills</div>
-                <ul className="home-menu">
-                    <li className="home-menu__li"><a className="home-menu__link home-menu__link--btn " href="/user/login">Login</a></li>
-                    <li className="home-menu__li"><a className="home-menu__link home-menu__link--btn" href="/user/signup">Join</a></li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/NavBar.scss";
+const NavBar = () => {
+  return (
+    <div className="home-nav one-edge-shadow">
+      <div className="home-row ">
+        <div className="home-logo">Ignio Skills</div>
+        <ul className="home-menu">
+          <li className="home-menu__li">
+            <Link className="home-menu__link home-menu__link--btn " to="/login">
+              Login
+            </Link>
+          </li>
+          <li className="home-menu__li">
+            <Link className="home-menu__link home-menu__link--btn" to="/signup">
+              Join
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar; 
+export default NavBar;
