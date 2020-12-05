d71f3e865b29275d998dc8f53d2725c419df1dc8
ghulamghousdev <itsghulamghous@gmail.com>
2020-12-06 00:19:56 +0500

    Integrated flexbox

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -6,6 +6,14 @@ const NavBar = () => {
     <div className="home-nav one-edge-shadow">
       <div className="home-row ">
         <div className="home-logo">Ignio Skills</div>
+        <form className="form-search">
+          <input 
+            className="form-search__input-search"
+            type="text" 
+            placeholder="Search"
+            name="search"
+          />
+        </form>
         <ul className="home-menu">
           <li className="home-menu__li">
             <Link className="home-menu__link home-menu__link--btn " to="/login">
