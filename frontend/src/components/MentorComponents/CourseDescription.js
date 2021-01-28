2f24089c20dc9e6457b7fa939514adf70fda8936
ghulamghousdev <itsghulamghous@gmail.com>
2021-01-28 14:38:49 +0500

    Added component for uploading course description

---
+++ frontend/src/components/MentorComponents/CourseDescription.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../../styles/CourseDescriptionPage.scss';
+import {Link} from 'react-router-dom';
+
+const CourseDescription = ({setDescription})=>{
+
+    return (
+        <div className= "course-container">
+            <form>
+                <h2 className='course-container__heading'>Give brief Description of your Course</h2>
+                <p>You can edit it anytime latter on so don't worry and give it a go.</p>
+                <textarea 
+                className="course-container__description"
+                type='text'
+                name='description'
+                placeholder="e.g. This course covers the basics of React..."
+                onChange={(e) => {
+                  setDescription(e.target.value)
+                }}
+                />
+            </form>
+            <Link className="course-container__previous-btn" to="/CreateCourse/Heading">
+              Previous
+            </Link>
+            <Link className="course-container__continue-btn" to={`/mentor/create-course/cover`}>
+              Continue
+            </Link>
+        </div>
+  );
+} 
+
+export default CourseDescription;
