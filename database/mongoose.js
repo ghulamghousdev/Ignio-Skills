42f60439d837b4366cf50a826fd373f931cd08cf
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-03-03 14:28:39 -0800

    Changed database

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //CONNECTING TO DATABASE
 mongoose
   .connect(
-    "mongodb://ghulamghousdev:ggatazfm@ignioskills-shard-00-00.jj1pk.mongodb.net:27017,ignioskills-shard-00-01.jj1pk.mongodb.net:27017,ignioskills-shard-00-02.jj1pk.mongodb.net:27017/elearning?ssl=true&replicaSet=atlas-bou4tb-shard-0&authSource=admin&retryWrites=true&w=majority",
+    "mongodb://ghulamghousdev:ggatazfm@ignioskills-shard-00-00.jj1pk.mongodb.net:27017,ignioskills-shard-00-01.jj1pk.mongodb.net:27017,ignioskills-shard-00-02.jj1pk.mongodb.net:27017/ignioSkills?ssl=true&replicaSet=atlas-bou4tb-shard-0&authSource=admin&retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
   )
   .then(() => {
