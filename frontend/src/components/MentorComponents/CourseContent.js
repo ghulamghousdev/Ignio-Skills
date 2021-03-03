fa559094007c37c00a4a1cc31aa772c88e14af1b
ghulamghousdev <itsghulamghous@gmail.com>
2021-03-03 21:01:48 +0500

    added component for uploading content

---
+++ frontend/src/components/MentorComponents/CourseContent.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+const AddVideo = () => {
+
+  const {data, setData} = useState(null);
+
+  componentDidMount(){
+    fetchData();
+  }
+
+  const fetchData = async () => {
+    try{
+      const config = {
+        headers: {
+            'Content-Type': 'Application/json',
+        }
+      }
+      const res = await axios.get('/api/subjects', config);
+      this.setState((prevState, props)=>{
+        return ({
+          subjects: prevState.subjects.concat(res.data)
+        })
+      }, ()=>{
+        console.log(this.state);
+      })
+    } catch(err){
+      console.log(err)
+    }
+  }
+    
+    return (
+        <div className= "course-container">
+            <div> Add Videos</div>
+            <Link className="course-container__continue-btn" to={`/mentor/create-course/content/video`}>
+              Add Video
+            </Link>
+        </div>
+  );
+}
+
+export default AddVideo;
