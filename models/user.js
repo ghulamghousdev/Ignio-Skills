36056a34bf44f84966d73131dc0aaa40bb3c8cfc
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-03-03 14:38:56 -0800

    Minor Change

--- models/user.js
+++ models/user.js
@@ -103,4 +103,6 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
