c85b2bc4b901199ff14853ce947264702a4086c1
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-03-03 14:39:34 -0800

    Minor Change

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,23 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-
-const auth = async(req, res, next) =>
-{
-    try{
-        const authToken = req.header('authorization').replace('Bearer', '');
-        const decoded = jwt.verify(authToken, "thisisuserverification");
-        const user = await User.findOne({
-            _id: decoded._id, 'tokens.token': token 
-        })
-        if(!user) {
-            throw new Error('Please Authenticate');
-        }
-        req.user = user;
-        req.token = authToken;
-        next();
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+//AUTHENTICATION MIDDLEWARE
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, "thisisuserverification");
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+    if (!user) {
+      throw new Error("Please Authenticate");
     }
-    catch(error){
-        res.status(400).send({error: 'Please Authenticate'});
-    }
-}
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (e) {
+    res.status(400).send({ error: "Please Authenticate" });
+  }
+};
 
 module.exports = auth;
