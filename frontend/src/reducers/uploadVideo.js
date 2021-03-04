92114be4d47ed314aaddb9be87044a750b2e4f95
ghulamghousdev <itsghulamghous@gmail.com>
2021-03-04 22:37:51 +0500

    Added reducer for upload video

---
+++ frontend/src/reducers/uploadVideo.js
@@ -0,0 +1,30 @@
+import {
+  UPLOAD_VIDEO_SUCCESS,
+  UPLOAD_VIDEO_FAIL,
+} from "../actions/actionTypes.js";
+
+const initialState = {
+  data: null,
+  isLoading: true,
+};
+
+const upload = (upload = initialState, action) => {
+  const { type, payload } = action;
+  if (type === UPLOAD_VIDEO_SUCCESS) {
+    return {
+      ...upload,
+      isLoading: false,
+      data: payload.data,
+    };
+  } else if (type === UPLOAD_VIDEO_FAIL) {
+    return {
+      ...upload,
+      data: null,
+      isLoading: false,
+    };
+  } else {
+    return upload;
+  }
+};
+
+export default upload;
