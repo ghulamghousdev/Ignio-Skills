1468b9db94567d95af259cea58e3f8528638cc93
ghulamghousdev <itsghulamghous@gmail.com>
2021-03-08 00:46:11 +0500

    Combined the reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from "redux";
 import auth from "./auth";
 import uploadVideo from "./uploadVideo";
 import course from "./course";
+import getCourses from "./loadCourses";
 
-const rootReducer = combineReducers({ auth, course, uploadVideo });
+const rootReducer = combineReducers({ auth, course, uploadVideo, getCourses });
 
 export default rootReducer;
