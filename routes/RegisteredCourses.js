a1fd8e208c0c49921073d5f6e9e879bb4b7a5936
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-03-07 12:05:02 -0800

    Added routes for Registered Courses

---
+++ routes/RegisteredCourses.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const RegisteredCourses = require("../models/RegisteredCourses");
+
+router.post("/courses/register/:id", async (req, res) => {
+  const body = {
+    user: req.user._id,
+    courseId: req.params.id,
+  };
+  const registeredCourse = new RegisteredCourses(body);
+  try {
+    await registeredCourse.save();
+    res.status(200).send(registeredCourse);
+  } catch (err) {
+    res.status(400).send({ err });
+  }
+});
+
+router.get("/courses", async (req, res) => {
+  try {
+    const registeredCourses = await RegisteredCourses.find({
+      user: req.user._id,
+    });
+    if (!registeredCourses) {
+      res.status(404).send({ err: "You have not registered any courses" });
+    }
+    await registeredCourses.populate("user").execPopulate();
+    await registeredCourses.populate("course").execPopulate();
+    res.status(200).send(registeredCourses);
+  } catch (err) {
+    res.status(400).send({ err });
+  }
+});
