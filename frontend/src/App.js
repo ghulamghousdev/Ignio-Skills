2146acc5748405fd449a59b9fa37b1ed6f9b0ade
ghulamghousdev <itsghulamghous@gmail.com>
2021-03-08 18:26:55 +0500

    resolved conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,12 +9,9 @@ import Homepage from "./components/Homepage";
 import MentorDashboard from "./components/MentorComponents/MentorDashboard";
 import CourseView from "./components/MentorComponents/CourseViewMentor";
 import StudentDashboard from "./components/StudentComponents/Dashboard";
-<<<<<<< HEAD
 import Join from "./components/Chatbox/Join";
 import Chat from "./components/Chatbox/Chat";
-=======
 import Dashboard from "./components/MentorComponents/Dashboard";
->>>>>>> 450c60e00db7742c305c2c8fb119674111dbef2d
 
 const App = () => {
   return (
@@ -27,14 +24,11 @@ const App = () => {
           <Route path="/homepage" component={Homepage} />
           <Route path="/mentor" component={MentorDashboard} />
           <Route path="/student" component={StudentDashboard} />
-<<<<<<< HEAD
           <Route path="/join" component={Join} exact />
           <Route path="/chat" component={Chat} />
 
-=======
           <Route path="/mentordashboard" component={Dashboard} />
           <Route path="/editcourse/:id" component={CourseView} />
->>>>>>> 450c60e00db7742c305c2c8fb119674111dbef2d
           <Route path="/" component={Homepage} />
         </Switch>
       </BrowserRouter>
